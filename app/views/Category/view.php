e22d3dd31203bce064c6717d11880c6f4601ed01
Yosia <yosiaproger@gmail.com>
2021-02-04 22:23:45 +0300

    Create category

--- app/views/Category/view.php
+++ app/views/Category/view.php
@@ -4,7 +4,7 @@
         <div class="breadcrumbs-main">
             <ol class="breadcrumb">
                 <li><a href="<?=PATH;?>">Главная</a></li>
-                <li>Поиск по запросу "<?=h($query);?>"</li>
+                <li>Категория</li>
             </ol>
         </div>
     </div>
@@ -18,39 +18,39 @@
                 <?php $curr = \shop\App::$app->getProperty('currency'); ?>
                 <?php $count = 0; ?>
                 <?php if(!empty($products)): ?>
-                        <?php foreach($products as $product): ?>
-                <?php if($count % 3 == 0 || $count == 0): ?>
-                        <div class="product-one">
-                            <?php endif; ?>
-                            <div class="col-md-4 product-left p-left">
-                                <div class="product-main simpleCart_shelfItem">
-                                    <a href="product/<?=$product->alias;?>" class="mask"><img class="img-responsive zoom-img" src="public/images/<?=$product->img;?>" alt="" /></a>
-                                    <div class="product-bottom">
-                                        <h3><?=$product->title;?></h3>
-                                        <p>Explore Now</p>
-                                        <h4>
-                                            <a data-id="<?=$product->id;?>" class="add-to-cart-link" href="cart/add?id=<?=$product->id;?>"><i></i></a> <span class=" item_price"><?=$curr['symbol_left'];?><?=$product->price * $curr['value'];?><?=$curr['symbol_right'];?></span>
-                                            <?php if($product->old_price): ?>
-                                                <small><del><?=$curr['symbol_left'];?><?=$product->old_price * $curr['value'];?><?=$curr['symbol_right'];?></del></small>
-                                            <?php endif; ?>
-                                        </h4>
-                                    </div>
-                                    <div class="srch srch1">
-                                        <span>-50%</span>
-                                    </div>
+                    <?php foreach($products as $product): ?>
+                        <?php if($count % 3 == 0 || $count == 0): ?>
+                            <div class="product-one">
+                        <?php endif; ?>
+                        <div class="col-md-4 product-left p-left">
+                            <div class="product-main simpleCart_shelfItem">
+                                <a href="product/<?=$product->alias;?>" class="mask"><img class="img-responsive zoom-img" src="public/images/<?=$product->img;?>" alt="" /></a>
+                                <div class="product-bottom">
+                                    <h3><?=$product->title;?></h3>
+                                    <p>Explore Now</p>
+                                    <h4>
+                                        <a data-id="<?=$product->id;?>" class="add-to-cart-link" href="cart/add?id=<?=$product->id;?>"><i></i></a> <span class=" item_price"><?=$curr['symbol_left'];?><?=$product->price * $curr['value'];?><?=$curr['symbol_right'];?></span>
+                                        <?php if($product->old_price): ?>
+                                            <small><del><?=$curr['symbol_left'];?><?=$product->old_price * $curr['value'];?><?=$curr['symbol_right'];?></del></small>
+                                        <?php endif; ?>
+                                    </h4>
+                                </div>
+                                <div class="srch srch1">
+                                    <span>-50%</span>
                                 </div>
                             </div>
-                        <?php $count++; ?>
-                            <?php if($count % 3 == 0 || $count == 0): ?>
-                            <div class="clearfix"></div>
                         </div>
+                        <?php $count++; ?>
+                        <?php if($count % 3 == 0): ?>
+                            <div class="clearfix"></div>
+                            </div>
                         <?php endif; ?>
-                        <?php endforeach; ?>
+                    <?php endforeach; ?>
                 <?php else: ?>
                     <h3>Товар отсутсвует</h3>
                 <?php endif; ?>
             </div>
-
+            </div>
             <div class="col-md-3 prdt-right">
                 <div class="w_sidebar">
                     <section  class="sky-form">
@@ -131,7 +131,6 @@
                 </div>
             </div>
             <div class="clearfix"></div>
-        </div>
         </div>
     </div>
 </div>
